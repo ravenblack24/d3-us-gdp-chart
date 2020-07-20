f5f38be7eb6a4214f90a991568307706acc2bc1d
Kay Pea <19981420+ravenblack24@users.noreply.github.com>
2020-07-20 14:06:35 +0100

    Axis complete

--- chart.js
+++ chart.js
@@ -5,72 +5,87 @@ document.addEventListener('DOMContentLoaded', function() {
     req.send();
     req.onload = function() {
        const dataset = JSON.parse(req.responseText);
-      const dTest = dataset.data;
+        const dTest = dataset.data;
 
-    //   const datasetTest = { "data": [
-    //     [
-    //       "1945-01-01",
-    //       100
-    //     ],
-    //     [
-    //       "1950-04-01",
-    //       150
-    //     ],
-    //     [
-    //       "1955-07-01",
-    //       50
-    //     ],
-    //     [
-    //       "1960-10-01",
-    //       200
-    //     ],
-    //     [
-    //       "1965-01-01",
-    //       300
-    //     ]
-    //   ]}
+      // const datasetTest = { "data": [
+      //   [
+      //     "1945-01-01",
+      //     100
+      //   ],
+      //   [
+      //     "1950-04-01",
+      //     150
+      //   ],
+      //   [
+      //     "1955-07-01",
+      //     50
+      //   ],
+      //   [
+      //     "1960-10-01",
+      //     200
+      //   ],
+      //   [
+      //     "1965-01-01",
+      //     300
+      //   ]
+      // ]}
 
-    //   const dTest = datasetTest.data;
+      // const dTest = datasetTest.data;
       
+      const xMax = new Date(d3.max(dTest, (item) => item[0])).getFullYear();
+      const xMin = new Date(d3.min(dTest, (item) => item[0])).getFullYear();
 
-      const max = d3.max(dTest, (item) => item[1]);
-      const min = d3.min(dTest, (item) => item[1]);
+      console.log(xMin, xMax)
+
+      const yMax = d3.max(dTest, (item) => item[1]);
+      const yMin = d3.min(dTest, (item) => item[1]);
     
-      const w = 900;
-      const h = 350;
+      const w = 830;
+      const h = 450;
+      const padding = 40;
 
       var xScale = d3.scaleLinear()
-                        .domain([0, dTest.length])
-                        .range(0, w);
-
-                        console.log(dTest.length);
+                      .domain([xMin, xMax])
+                      .range([padding, w-padding]);
 
       var yScale = d3.scaleLinear()
-                        .domain([0, max])
-                        .range([0, h]);
+                      .domain([yMin, yMax])
+                      .range([h-padding, padding]);
   
-      const body = d3.select(".chart")
+      const svg = d3.select(".chart")
                  .append("svg")
                  .attr("width", w)
                  .attr("height", h)
                  .attr("class", "chart__svg");
    
       
-    body.selectAll("rect")
+      svg.selectAll("rect")
         .data(dTest)
         .enter()
         .append("rect")
         .attr("data-date", (dTest) => dTest[0])
         .attr("data-gdp", (dTest) => dTest[1])
-        .attr("width", 2)
-        .attr("height", (dTest) => yScale(dTest[1]))
-        .attr("x", (dTest, i) =>  i*3)
-        .attr("y", (dTest) => h -  yScale(dTest[1]))
+        .attr("x", (dTest) => xScale(new Date(dTest[0]).getFullYear()))
+        .attr("y", (dTest) => yScale(dTest[1]))
+        .attr("width", w/dTest.length)
+        .attr("height", (dTest) => (h - padding) - yScale(dTest[1]))
         .attr("fill", "navy")
         .attr("class", "bar");
 
+      const xAxis = d3.axisBottom(xScale);
+      const yAxis = d3.axisLeft(yScale);
+
+      svg.append("g")
+        .attr("id", "xAxis")
+        .attr("transform", "translate(0," + (h - padding) + ")")
+        .attr("fill", "black")
+        .call(xAxis);
       
-      
+      svg.append("g")
+        .attr("id", "yAxis")
+        .attr("transform", "translate("+padding+", 0)")
+        .attr("fill", "black")
+        .call(yAxis);
       
     }
   });
